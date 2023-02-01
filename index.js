e0471bbffe01bad906a0ea4d69cba7552f50426c
Gage Harmon <gaharmon1@gmail.com>
2023-02-01 10:22:01 -0700

    filter method

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution here
+function findMatching(d, found) {
+    return d.filter((matchingDriver) => matchingDriver.toUpperCase().includes(found.toUpperCase()));
+}
+
+
+function fuzzyMatch(d, firstLetter) {
+    return d.filter((matchingDriver) => matchingDriver.indexOf(firstLetter) === 0);
+}
+
+
+function matchName(d, foundName) {
+    return d.filter((dirt) => dirt.name === foundName);
+}
